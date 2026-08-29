1a4235ab4a2da2562d3f04d351ff0335327e6286
Zachary Huff <zach.huff.386@gmail.com>
2026-08-29 18:51:00 -0400

    Update import in cli add cmd

--- cli/cmd/add.go
+++ cli/cmd/add.go
@@ -1,31 +1,19 @@
 package cmd
 
 import (
-	"strings"
-
 	"github.com/pritunl/pritunl-client/cli/sprofile"
 	"github.com/spf13/cobra"
 )
 
 var AddCmd = &cobra.Command{
-	Use:   "add [profile_uri|tar_path]",
+	Use:   "add [profile_uri|tar_path|ovpn_path]",
 	Short: "Add profile",
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) == 0 {
 			cobra.CheckErr("cmd: Missing profile URI or path")
 		}
 
-		path := args[0]
-		if strings.HasPrefix(path, "http://") ||
-			strings.HasPrefix(path, "https://") ||
-			strings.HasPrefix(path, "pritunl://") ||
-			strings.HasPrefix(path, "pritunls://") {
-
-			err := sprofile.ImportUri(path)
-			cobra.CheckErr(err)
-		} else {
-			err := sprofile.ImportTar(path)
-			cobra.CheckErr(err)
-		}
+		err := sprofile.ImportPath(args[0])
+		cobra.CheckErr(err)
 	},
 }
