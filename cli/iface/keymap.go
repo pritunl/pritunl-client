6669b9f412fc8e201a3703ab0aeefd25ca551437
Zachary Huff <zach.huff.386@gmail.com>
2026-08-29 19:05:49 -0400

    Update bindings in cli iface keymap

--- cli/iface/keymap.go
+++ cli/iface/keymap.go
@@ -13,6 +13,8 @@ type KeyMap struct {
 	Import     key.Binding
 	Logs       key.Binding
 	Settings   key.Binding
+	Remove     key.Binding
+	Refresh    key.Binding
 	Quit       key.Binding
 }
 
@@ -30,12 +32,12 @@ var bindings = KeyMap{
 		key.WithHelp("i", "import"),
 	),
 	Connect: key.NewBinding(
-		key.WithKeys("c"),
-		key.WithHelp("c", "connect_ovpn"),
+		key.WithKeys("c", "enter"),
+		key.WithHelp("c", "connect"),
 	),
 	ConnectWg: key.NewBinding(
 		key.WithKeys("w"),
-		key.WithHelp("w", "connect_wg"),
+		key.WithHelp("w", "connect wireguard"),
 	),
 	Disconnect: key.NewBinding(
 		key.WithKeys("d"),
@@ -49,8 +51,47 @@ var bindings = KeyMap{
 		key.WithKeys("s"),
 		key.WithHelp("s", "settings"),
 	),
+	Remove: key.NewBinding(
+		key.WithKeys("r", "delete"),
+		key.WithHelp("r", "remove"),
+	),
+	Refresh: key.NewBinding(
+		key.WithKeys("R", "ctrl+r"),
+		key.WithHelp("R", "refresh"),
+	),
 	Quit: key.NewBinding(
 		key.WithKeys("q", "ctrl+c"),
 		key.WithHelp("q", "quit"),
+	),
+}
+
+type LogsKeyMap struct {
+	Back  key.Binding
+	Clear key.Binding
+	Top   key.Binding
+	End   key.Binding
+	Quit  key.Binding
+}
+
+var logsKeys = LogsKeyMap{
+	Back: key.NewBinding(
+		key.WithKeys("esc", "q", "l", "backspace"),
+		key.WithHelp("esc", "back"),
+	),
+	Clear: key.NewBinding(
+		key.WithKeys("c"),
+		key.WithHelp("c", "clear"),
+	),
+	Top: key.NewBinding(
+		key.WithKeys("home", "g"),
+		key.WithHelp("home", "top"),
+	),
+	End: key.NewBinding(
+		key.WithKeys("end", "G"),
+		key.WithHelp("end", "end"),
+	),
+	Quit: key.NewBinding(
+		key.WithKeys("ctrl+c"),
+		key.WithHelp("ctrl+c", "quit"),
 	),
 }
