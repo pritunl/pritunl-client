5fbece1587fa6c585d6cde78e9331b974e8bd8cf
Zachary Huff <zach.huff.386@gmail.com>
2026-09-02 10:33:02 -0400

    Add flatpak to client constants

--- client/main/Constants.ts
+++ client/main/Constants.ts
@@ -4,10 +4,7 @@ import os from "os"
 import process from "process"
 
 export let unix = false
-export const unixPath = "/var/run/pritunl.sock"
 export const webHost = "http://127.0.0.1:9770"
-export const unixWsHost = "ws+unix://" + path.join(
-	path.sep, "var", "run", "pritunl.sock") + ":"
 export const webWsHost = "ws://127.0.0.1:9770"
 export const platform = os.platform()
 export const hostname = os.hostname()
@@ -18,6 +15,20 @@ export let mainWindow: electron.BrowserWindow
 
 export let production = (process.argv.indexOf("--dev") === -1)
 export let devTools = (process.argv.indexOf("--dev-tools") !== -1)
+export let flatpak = process.env.FLATPAK_MODE === "true"
+
+export const flatpakId = process.env.FLATPAK_ID || "com.pritunl.Client"
+export const flatpakRunDir = path.join(
+	process.env.XDG_RUNTIME_DIR || "", "app", flatpakId)
+export let flatpakError = ""
+if (flatpak && !process.env.XDG_RUNTIME_DIR) {
+	flatpakError = "XDG_RUNTIME_DIR not defined in flatpak mode"
+}
+
+export const unixPath = flatpak ?
+	path.join(flatpakRunDir, "pritunl.sock") :
+	path.join(path.sep, "var", "run", "pritunl.sock")
+export const unixWsHost = "ws+unix://" + unixPath + ":"
 
 export let winDrive = "C:\\"
 let systemDrv = process.env.SYSTEMDRIVE
@@ -27,6 +38,15 @@ if (systemDrv) {
 
 if (process.platform === "linux" || process.platform === "darwin") {
 	unix = true
+}
+
+export let authPath = ""
+if (process.platform === "win32") {
+	authPath = path.join(winDrive, "ProgramData", "Pritunl", "auth")
+} else if (flatpak) {
+	authPath = path.join(flatpakRunDir, "pritunl.auth")
+} else {
+	authPath = path.join(path.sep, "var", "run", "pritunl.auth")
 }
 
 export function setMainWindow(mainWin: electron.BrowserWindow) {
