66e2d26dc495ee4c31d2a4087552c5b446e08308
Zachary Huff <zach.huff.386@gmail.com>
2026-09-02 05:20:29 -0400

    Add flatpak to client build remote

--- client/build_remote.sh
+++ client/build_remote.sh
@@ -4,14 +4,17 @@ set -e
 rsync --human-readable --archive --xattrs --progress --delete --exclude "/node_modules/*" --exclude "/jspm_packages/*" --exclude "app/*.js" --exclude "app/*.js.map" --exclude "app/**/*.js" --exclude "app/**/*.js.map" /home/cloud/git/pritunl-client/client/ $NPM_SERVER:/home/cloud/pritunl-client-www/
 
 ssh cloud@$NPM_SERVER "
+set -e
+export PATH=\$HOME/.local/bin:\$PATH
 cd /home/cloud/pritunl-client-www/
 rm -rf node_modules
-npm install --min-release-age=3
-npm audit fix
+npm install
+flatpak-node-generator npm package-lock.json -o generated-sources.json
 "
 
 scp $NPM_SERVER:/home/cloud/pritunl-client-www/package.json /home/cloud/git/pritunl-client/client/package.json
 scp $NPM_SERVER:/home/cloud/pritunl-client-www/package-lock.json /home/cloud/git/pritunl-client/client/package-lock.json
+scp $NPM_SERVER:/home/cloud/pritunl-client-www/generated-sources.json /home/cloud/git/pritunl-client/flatpak/generated-sources.json
 rsync --human-readable --archive --xattrs --progress --delete $NPM_SERVER:/home/cloud/pritunl-client-www/node_modules/ /home/cloud/git/pritunl-client/client/node_modules/
 rsync --human-readable --archive --xattrs --progress --delete --exclude "/node_modules/*" --exclude "/jspm_packages/*" --exclude "app/*.js" --exclude "app/*.js.map" --exclude "app/**/*.js" --exclude "app/**/*.js.map" /home/cloud/git/pritunl-client/client/ $NPM_SERVER:/home/cloud/pritunl-client-www/
 
