883b54c2b77eb3c3bce2e688277a69d85b4e1eb9
Zachary Huff <zach.huff.386@gmail.com>
2026-09-02 09:47:34 -0400

    Update flatpak paths in client auth

--- client/main/Auth.ts
+++ client/main/Auth.ts
@@ -1,27 +1,10 @@
 import fs from "fs";
-import process from "process";
-import path from "path";
-import {winDrive} from "./Service";
+import * as Constants from "./Constants";
 
 export let token = '';
-export let unix = false
-export const unixPath = "/var/run/pritunl.sock"
-export const webHost = "http://127.0.0.1:9770"
-
-if (process.platform === "linux" || process.platform === "darwin") {
-	unix = true
-}
-
-function getAuthPath(): string {
-	if (process.platform === "win32") {
-		return path.join(winDrive, "ProgramData", "Pritunl", "auth")
-	} else {
-		return path.join(path.sep, "var", "run", "pritunl.auth")
-	}
-}
 
 export function _load(): void {
-	fs.readFile(getAuthPath(), 'utf-8', (err, data: string): void => {
+	fs.readFile(Constants.authPath, 'utf-8', (err, data: string): void => {
 		if (err || !data) {
 			setTimeout((): void => {
 				_load();
@@ -39,7 +22,7 @@ export function _load(): void {
 
 export function load(): Promise<void> {
 	return new Promise<void>((resolve, reject): void => {
-		fs.readFile(getAuthPath(), 'utf-8', (err, data: string): void => {
+		fs.readFile(Constants.authPath, 'utf-8', (err, data: string): void => {
 			if (err || !data) {
 				setTimeout((): void => {
 					_load();
