671c5f351ae882e0a0d90faf66eb76e8af5fd659
Zachary Huff <zach.huff.386@gmail.com>
2026-09-03 04:52:52 -0400

    Add flatpak to service constants

--- service/constants/constants.go
+++ service/constants/constants.go
@@ -1,5 +1,9 @@
 package constants
 
+import (
+	"os"
+)
+
 const (
 	Version        = "1.3.4729.52"
 	MacosHelperDir = "/Library/PrivilegedHelperTools/pritunl-client"
@@ -9,4 +13,5 @@ var (
 	Development = false
 	Macos10     = false
 	Interrupt   = false
+	Flatpak     = os.Getenv("FLATPAK_MODE") == "true"
 )
