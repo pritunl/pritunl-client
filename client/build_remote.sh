1a2c06f6bd94c650d69cb7220a14e92400690c41
Zachary Huff <zach.huff.386@gmail.com>
2026-09-06 10:31:21 -0400

    Update client build remote

--- client/build_remote.sh
+++ client/build_remote.sh
@@ -15,6 +15,8 @@ flatpak-node-generator npm package-lock.json -o generated-sources.json
 scp $NPM_SERVER:/home/cloud/pritunl-client-www/package.json /home/cloud/git/pritunl-client/client/package.json
 scp $NPM_SERVER:/home/cloud/pritunl-client-www/package-lock.json /home/cloud/git/pritunl-client/client/package-lock.json
 scp $NPM_SERVER:/home/cloud/pritunl-client-www/generated-sources.json /home/cloud/git/pritunl-client/flatpak/generated-sources.json
+python3 /home/cloud/git/pritunl-client/tools/flatpak_go.py /home/cloud/git/pritunl-client/service --dest-prefix service --out-dir /home/cloud/git/pritunl-client/flatpak --name service
+python3 /home/cloud/git/pritunl-client/tools/flatpak_go.py /home/cloud/git/pritunl-client/cli --dest-prefix cli --out-dir /home/cloud/git/pritunl-client/flatpak --name cli
 rsync --human-readable --archive --xattrs --progress --delete $NPM_SERVER:/home/cloud/pritunl-client-www/node_modules/ /home/cloud/git/pritunl-client/client/node_modules/
 rsync --human-readable --archive --xattrs --progress --delete --exclude "/node_modules/*" --exclude "/jspm_packages/*" --exclude "app/*.js" --exclude "app/*.js.map" --exclude "app/**/*.js" --exclude "app/**/*.js.map" /home/cloud/git/pritunl-client/client/ $NPM_SERVER:/home/cloud/pritunl-client-www/
 
